4734d59737bf2c3aa1b27a41874fba76bec5d691
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-11-01 21:44:12 +0100

    add(Inventory): add PropTypes

--- src/Components/Inventory.js
+++ src/Components/Inventory.js
@@ -1,32 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 import InvItem from './InvItem';
 
-class Inventory extends Component {
-  render() {
-    let items = this.props ? this.props.items : [];
-
-    let itemList = [];
-    for (let j = 0; j < 9; j++) {
-      if (items.length >= j + 1) {
-        itemList.push(items[j]);
+const Inventory = ({ items = [], styleGuide }) => {
+  let itemList = [];
+  for (let j = 0; j < 9; j++) {
+    if (items.length >= j + 1) {
+      itemList.push(items[j]);
+    } else {
+      if (j === 0 && items.length < 1) {
+        //itemList.push({name: "", equipped: false, selected: true});
       } else {
-        if (j === 0 && items.length < 1) {
-          //itemList.push({name: "", equipped: false, selected: true});
-        } else {
-          itemList.push(null);
-        }
+        itemList.push(null);
       }
     }
-
-    let elems = itemList.map((n, i) => {
-      return <InvItem key={i} styleGuide={this.props.styleGuide} data={itemList[i]}/>
-    });
-
-    return <div className="inv" align="center">
-      {elems}
-    </div>
-  
   }
+
+  return (
+    <div className="inv" align="center">
+      {itemList.map((item, index) =>
+        <InvItem key={index} styleGuide={styleGuide} data={item}/>
+      )}
+    </div>
+  )
+}
+
+Inventory.propTypes = {
+  items: PropTypes.array,
+  styleGuide: PropTypes.object.isRequired
+  // TODO: use more specific types (e.g.: PropTypes.arrayOf(PropTypes.shape({...})) )
 }
 
 export default Inventory;
