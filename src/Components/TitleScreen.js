61de440f8eadd7aea3ba3ae097c4ad4b9ed961a9
Adrien Boutigny <adrien.boutigny@gmail.com>
2018-11-01 19:59:32 +0100

    add(TitleScreen): add propTypes field

--- src/Components/TitleScreen.js
+++ src/Components/TitleScreen.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class TitleScreen extends Component {
+  static propTypes = {
+    startgame: PropTypes.func.isRequired,
+    winner: PropTypes.string,
+    killedBy: PropTypes.string
+  }
+
   startgame(e){
     e.preventDefault();
     let div = document.getElementById('name');
@@ -21,38 +28,41 @@ class TitleScreen extends Component {
   }
 
   render() {
-    let killedBy = this.props.killedBy;
+    const { killedBy, winner } = this.props
+    let message = null
     if (killedBy) {
-      killedBy = "Killed by: " + killedBy;
-    } else if (this.props.winner) {
-      killedBy = this.props.winner + " is the Winning Wizard!";
+      message = "Killed by: " + killedBy;
+    } else if (winner) {
+      message = winner + " is the Winning Wizard!";
     }
-    return (<div className = 'title-screen' style={{backgroundColor: "#000000", color:"#FFFFFF"}}>
-      <h1>WizardWars.io</h1>
-      <h3>A Roguelike Deathmatch</h3>
-      <div>{killedBy}</div>
-      <form onSubmit={this.startgame.bind(this)}> <br/>
-      <input id='name'></input>
-      <br/>
-      <input type="submit" value="Join Game"/>
-      </form><p/>
-      Choose a name. <br/>
-      MOVE with numpad or arrow keys. <br/>
-      STOP with space or numpad5.<br/>
-      READY item in inventory with 1-9.<p/>
-      &nbsp;A - APPLY/EQUIP &nbsp; --> potions/weapons/armor/tools<br/>
-      &nbsp;S - SWITCH &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  a switch/turret<br/>
-      &nbsp;D - DROP&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--> item<br/>
-      &nbsp;F - FLING/FIRE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; --> item/zap wand
-      <p/>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/WizardWarsIO/wiz-server">Tutorial + Source Code</a>
-      <br/>
-      <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/7nJNw2T">Discord Player Chat</a>
-      <br/>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.patreon.com/WizardWars">Patreon Support</a>
-      <br/>
-    </div>
-  )
+    return (
+      <div className = 'title-screen' style={{backgroundColor: "#000000", color:"#FFFFFF"}}>
+        <h1>WizardWars.io</h1>
+        <h3>A Roguelike Deathmatch</h3>
+        <div>{message}</div>
+        <form onSubmit={this.startgame.bind(this)}> <br/>
+          <input id='name'></input>
+          <br/>
+          <input type="submit" value="Join Game"/>
+        </form>
+        <p/>
+          Choose a name. <br/>
+          MOVE with numpad or arrow keys. <br/>
+          STOP with space or numpad5.<br/>
+          READY item in inventory with 1-9.<p/>
+          &nbsp;A - APPLY/EQUIP &nbsp; --> potions/weapons/armor/tools<br/>
+          &nbsp;S - SWITCH &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  a switch/turret<br/>
+          &nbsp;D - DROP&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;--> item<br/>
+          &nbsp;F - FLING/FIRE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; --> item/zap wand
+        <p/>
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/WizardWarsIO/wiz-server">Tutorial + Source Code</a>
+        <br/>
+        <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/7nJNw2T">Discord Player Chat</a>
+        <br/>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.patreon.com/WizardWars">Patreon Support</a>
+        <br/>
+      </div>
+    )
   }
 }
 export default TitleScreen;
